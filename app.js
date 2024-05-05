0bac7b5c6ba70e592a61a6a454bb34457f58059d
= <=>
2024-05-06 00:52:01 +0100

    complete the google oauth integration

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
 const express = require('express');
+const session = require("express-session");
+const dotenv = require('dotenv');
+dotenv.config();
+
+require('./auth');
+const passport = require('passport');
 const app = express();
 
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res)=>{
-    res.send('<a href= "auth/google">Authenticate with google </a>')
+    res.send('<a href= "auth/google?user_type=scout">Authenticate with google </a>')
 })
 
-app.get('/dashboard', (req, res)=>{
+function isLoggedIn (req, res, next) {
+    return req.user ? next() : res.sendStatus(401)
+}
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'], state: '/auth/google?user_type=scout' },))
+
+app.get('/google/callback', 
+    passport.authenticate('google', {
+        successRedirect: "/dashboard",
+        failureRedirect: '/auth/failure'
+    })
+);
+
+app.get("/auth/failure", (req, res)=>{
+    res.send("Something went wrong")
+})
+
+app.get('/dashboard', isLoggedIn, (req, res)=>{
+    console.log(req.user);
     res.send('Hello Segun, Welcome Home')
 })
 
-const port = 3080
+app.get('/logout', (req, res) => {
+    req.logout( ()=>{
+        req.session.destroy();
+    });
+    res.send('Goodbye')
+})
+
+const port = 3080;
 app.listen(port, () =>{
     console.log("Listening on localhost:"+port)
 })
--- auth.js
+++ auth.js
@@ -3,19 +3,36 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3080/google/callback",
     passReqToCallback: true
 },
-function(request, accessToken, refreshToken, profile, done){
-    return 
-}
+    function (req, request, accessToken, refreshToken, profile, done) {
+        const url = require('url');
 
-)
+        const state = req.query.state;
+        const parsedUrl = url.parse(state, true);
 
+        const user_type = parsedUrl.query.user_type;
+       
+        const user = {
+            profile,
+            user_type
+        };
+        return done(null, user);
+    }
+));
 
+passport.serializeUser(function (user, done) {
+    done(null, user)
+});
 
-)
+passport.deserializeUser(function (user, done) {
+    done(null, user)
+});
+
 
